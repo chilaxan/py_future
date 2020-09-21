c3830dc907dfd3798ec3c7b529dc536fbd7d6853
chilaxan <35645806+chilaxan@users.noreply.github.com>
2020-09-21 10:57:08 -0400

    Update expdict.py

--- expdict.py
+++ expdict.py
@@ -8,4 +8,4 @@ def dict_iter(self):
     last_frame = inspect.currentframe().f_back
     for inst in dis.Bytecode(last_frame.f_code):
         if inst.opname in ['STORE_FAST', 'STORE_GLOBAL', 'STORE_NAME', 'STORE_DEREF']:
-            yield self.get(instruction.argval)
+            yield self.get(inst.argval)
